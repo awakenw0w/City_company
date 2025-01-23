1aceb079dd72d28f2cdcabe7131e685123a83439
Danil <den4ikpvpyt10@gmail.com>
2025-01-23 13:53:17 +0300

    Update Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String[] args) {
-        Company ob = new Company();
-        ob.show();
-        AvtoCompany ob1 = new AvtoCompany();
-        ob1.show();
-    }
-}
