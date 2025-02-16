fc9ac42f425c6e81a24c79cb783247fd573d9742
awakenw0w <awakenwow42@gmail.com>
2025-02-16 14:28:54 +0300

    Update Company.java

--- src/Company.java
+++ src/Company.java
@@ -34,48 +34,5 @@ abstract class Company {
     }
 }
 
-class AvtoCompany extends Company { //автомобильная компания
-    int count; // колво автомобилей
-    int haul; //объем грузоперевозок
 
-    public AvtoCompany(String name, int persons, int money, int count, int haul) { // конструктор
-        super(name, persons, money);
-        this.count = count;
-        this.haul = haul;
-    }
 
-    //методы
-    //определение среднего объема грузоперевозок
-    public int averageAvto() {
-        return haul / count;
-    }
-}
-
-class InsCompany extends Company{ // страховая компания
-    int counts; //колво застрахованных лиц
-    int summaplus; // сумма страховых взносов
-    int summaminus; //сумма страховых выплат
-
-    //конструктор
-    public InsCompany(String name, int persons, int money, int counts, int summaminus, int summaplus){
-        super(name, persons, money); // вызывает конструктор родительского класса
-        this.counts = counts;
-        this.summaplus = summaplus;
-        this.summaminus = summaminus;
-    }
-
-    //свойства
-    public int getSummaplus() { // выводим сумма страховых взносов
-        return summaplus;
-    }
-    // методы
-    public int averageInsSumma(){ //вычисление среднего размера страховых взносов
-        return summaplus/counts;
-    }
-    public void show(){
-        super.show();
-        System.out.println("Застрахованных лиц: " + counts + ". Взносы: " + summaplus + ". Выплаты: " + summaminus);
-
-    }
-
-}
